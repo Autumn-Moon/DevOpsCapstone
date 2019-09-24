d80a83c0226e0f14dd81509a1ab3b9b15d61e4cf
EC2 Default User <ec2-user@ip-10-0-1-31.ec2.internal>
2019-09-24 13:20:08 +0000

    add docker push script

---
+++ docker_push.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#-----------------------------------------------------------------------------------------------------#
+# Script Name: docker_push.sh                                                                         #
+# Date       : 2019-09-24                                                                             #
+# Purpose    : Push Docker Image to Nexus Repository                                                  #
+#-----------------------------------------------------------------------------------------------------#
+
+#-----------------------------------------------------#
+# Logging Function                                    #
+#-----------------------------------------------------#
+function Echo
+{
+  echo
+  echo `/bin/date` " ==> $1"
+  echo
+}
+
+NEXUSPORT=8085
+IPADDR=`curl http://169.254.169.254/latest/meta-data/public-ipv4`
+DOCKERIMAGENAME=${IPADDR}:${NEXUSPORT}/sowtracker:${JENKINSBUILDNO}
+
+docker login -u admin -p nexusadmin ${IPADDR}:8085
+Echo "Docker Login to Nexus Repository"
+
+docker push ${DOCKERIMAGENAME}
+Echo "Pushing Docker Image to Nexus Repository"
+
+exit 0
