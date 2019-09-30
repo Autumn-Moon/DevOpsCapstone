430d3630bac7e87f705f23060edea3fd402480a1
EC2 Default User <ec2-user@ip-10-0-1-31.ec2.internal>
2019-09-30 07:34:19 +0000

    add ansible deployment

--- create_env.sh
+++ create_env.sh
@@ -36,27 +36,30 @@ Echo "Starting the Nexus artifacts Repository"
 sudo su -c "sh /root/docker/docker_prepare.sh"
 Echo "Prepare the Docker Environment for Jenkins job"
 
-##Fetch Public IP Address of the Jenkins Machine"
+## Fetch Public IP Address of the Jenkins Machine"
 IPADDR=`curl http://169.254.169.254/latest/meta-data/public-ipv4`
 
-##Get JenkinsBuild no of the Jenkins Machine"
+## Get JenkinsBuild no of the Jenkins Machine"
 JENKINSBUILDNO=${BUILD_NUMBER}
 
 ##Port number of Nexus artifacts Repository
 NEXUSPORT=8085
 
-##Set Docker Image Name required for Nexus Repository and add to environment file for docker 
+## Set Docker Image Name required for Nexus Repository and add to environment file for docker
 DOCKERIMAGENAME=${IPADDR}:${NEXUSPORT}/sowtracker:${JENKINSBUILDNO}
 echo "DOCKERIMAGENAME=${IPADDR}:${NEXUSPORT}/sowtracker:${JENKINSBUILDNO}" > .env
 Echo "Setting Docker Image Name required for Nexus Repository"
 
-##Create variables file for ansible Playbook
+## Create variables file for ansible Playbook
 echo "---" > vars.yml
 echo "dockerimage: ${DOCKERIMAGENAME}" >> vars.yml
 echo "prv_reg_add: ${IPADDR}:${NEXUSPORT}" >> vars.yml
 echo "nexus_user: admin" >> vars.yml
 echo "nexus_secret: nexusadmin" >> vars.yml
 Echo "Creating Variables file for Ansible Playbook"
+
+sed -e "s/public_ip/${IPADDR}/g" ansiserver.dat > ansiserver
+Echo "Creating Inventory file with IP Address of DockerHost"
 
 Echo "Script create_env.sh exiting successfully"
 
